5e942f9c9066db60a1fb0e48c853965ce0a675f8
marc3lsk <marc3lsk@gmail.com>
2024-05-21 21:26:08 +0200

    magic calculator

---
+++ Core/Features/MagicCalculation/BusinessLogic/MagicCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Features.MagicCalculation.BusinessLogic;
+
+public static class MagicCalculator
+{
+    public static double CalculateOutputValue(double inputValue)
+    {
+        var naturalLog = Math.Log(inputValue);
+
+        var thirdRoot = Math.Pow(naturalLog, 1.0 / 3.0);
+
+        return thirdRoot;
+    }
+}
---
+++ Tests/UnitTests/Features/MagicCalculation/BusinessLogic/MagicCalculator.Tests.cs
@@ -0,0 +1,37 @@
+﻿using Core.Features.MagicCalculation.BusinessLogic;
+using FluentAssertions;
+using OfficeOpenXml;
+
+namespace UnitTests.Features.MagicCalculation.BusinessLogic;
+
+public class MagicCalculatorTests
+{
+    public static IEnumerable<object[]> GetTestData(string excelFilePath)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        using (var package = new ExcelPackage(new System.IO.FileInfo(excelFilePath)))
+        {
+            var worksheet = package.Workbook.Worksheets[0]; // Assuming data is in the first sheet
+            for (int row = 1; row <= worksheet.Dimension.End.Row; row++) // Skip header row
+            {
+                var input = worksheet.Cells[row, 1].Value; // Column A
+                var output = worksheet.Cells[row, 2].Value; // Column B
+                yield return new object[] { input, output };
+            }
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTestData), "./Features/MagicCalculation/BusinessLogic/MagicCalculatorTestData.xlsx", MemberType = typeof(MagicCalculatorTests))]
+    public void TestWithExcelValues(double inputValue, double outputValue)
+    {
+        Math.Round(MagicCalculator.CalculateOutputValue(inputValue), 5).Should().Be(outputValue);
+    }
+
+    [Theory]
+    [InlineData(0, double.PositiveInfinity)]
+    public void Zero_Should_Be_Positive_Infinity(double inputValue, double outputValue)
+    {
+        Math.Round(MagicCalculator.CalculateOutputValue(inputValue), 5).Should().Be(outputValue);
+    }
+}
